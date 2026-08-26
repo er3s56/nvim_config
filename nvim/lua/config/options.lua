c46749a208669fc0e0de8cfde545ab01fbcc7e8c
er3s56 <er3s56@hotmail.com>
2026-08-26 09:51:38 +0800

    fix: use stable absolute line numbers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Keep editor line numbers stable when the cursor moves. LazyVim enables
+-- relative numbers by default, which makes every number except the current
+-- line represent a distance rather than the file's actual line number.
+vim.opt.number = true
+vim.opt.relativenumber = false
